f87a04e46b838ac5bfccfe442a2e875890d3ceb6
AlexisCast <castellanos.alexis@hotmail.com>
2023-08-07 13:44:07 -0700

    callback chaining

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ dotenv.config();
 const geocode = require("./utils/goecode");
 const forecast = require("./utils/forecast");
 
-geocode("Boston", (error, data) => {
-	console.log("Error", error);
-	console.log("Data", data);
-});
+const address = process.argv[2];
 
-forecast(-75.7088, 44.1545, (error, data) => {
-	console.log("Error", error);
-	console.log("Data", data);
-});
+if (!address) {
+	console.log("No address was provided");
+} else {
+	geocode(address, (error, data) => {
+		if (error) {
+			return console.log("Error", error);
+		}
+		forecast(data.latitud, data.longitud, (error, forecatData) => {
+			if (error) {
+				return console.log("Error", error);
+			}
+			console.log(data.location);
+			console.log(forecatData);
+		});
+	});
+}
